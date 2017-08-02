01f58137f9c56b62d61e7f5ad700379ca9ca606d
刘亮 <liang.liu04@hand-china.com>
2017-08-02 20:45:02 +0800

    提交

---
+++ src/main/java/com/didispace/domain/UserRepository.java
@@ -0,0 +1,20 @@
+package com.didispace.domain;
+
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+@CacheConfig(cacheNames = "users")
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Cacheable(key = "#p0")
+    User findByName(String name);
+
+    @CachePut(key = "#p0.name")
+    User save(User user);
+
+}
